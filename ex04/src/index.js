a3f6da14d902a1897dbdecfa90a5baa4da264a32
basslines <admir.osmanovic@gmail.com>
2021-03-16 16:09:58 +0100

    dodao index.js fajl

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function compareDifferentValues(m, n) {
+  if (m === n) {
+    return "Equal";
+  } else {
+    return "Not equal";
+  }
+}
+
+console.log(compareDifferentValues(8, "8"));
+module.exports = compareDifferentValues;
