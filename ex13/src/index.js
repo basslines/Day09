36087a401ab75fc6dfd69750961318a8ec96e596
basslines <admir.osmanovic@gmail.com>
2021-03-16 21:49:02 +0100

    dodao index.js fajl

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function inverseWhile() {
+  var fiveNumbers = 5;
+
+  while (fiveNumbers > -1) {
+    console.log("String of " + fiveNumbers);
+    fiveNumbers--;
+  }
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
